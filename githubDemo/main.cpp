28ca74e9be686c2ff8787e0a6e98a16542b4293d
junevil0603 <79992819@qq.com>
2020-07-14 06:57:38 +0800

    测试：第二次修改

--- githubDemo/main.cpp
+++ githubDemo/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <string>
+#include <Windows.h>
 
-int main(void){
-	std::cout << " Hello world!" << std::endl;
+using namespace std;
+
+int main (void){
+	string name;
+	string pwd;
+
+	while(1){
+		system("cls");
+		cout <<"ÇëÊäÈëÕÊºÅ£º";
+		cin >>name;
+		cout <<"ÇëÊäÈëÃÜÂë£º";
+		cin >>pwd;
+
+		if(name == "54hk" && pwd == "123456"){
+			break;
+		}else{
+			cout << "ÄãÊäÈëµÄÕÊºÅ»òÃÜÂëÓÐÎóÇëÖØÐÂÊäÈë" << endl;
+			system("pause");
+		}
+	}
+	
+	system("cls");
+	cout << "1.ÍøÕ¾404¹¥»÷" << endl;
+	cout << "2.ÍøÕ¾´Û¸Ä¹¥»÷" << endl;
+	cout << "3.ÍøÕ¾¹¥»÷¼ÇÂ¼" << endl;
+	cout << "4.DNS¹¥»÷" << endl;
+	cout << "5.·þÎñÆ÷ÖØÆô¹¥»÷" << endl;
+	
+	system("pause");
 	return 0;
 }
